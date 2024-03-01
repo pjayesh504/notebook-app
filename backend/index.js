f0e1032fb73ecdd82455f518b60f49809eeed158
pjayesh504 <pjayesh504@gmail.com>
2024-03-01 19:13:52 +0530

    Creating Routes and Schema

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,18 @@
 const connectToMongo = require('./db');
-const express = require('express');
 connectToMongo();
+const express = require('express')
 
 const app = express()
 const port = 3000
 
+//Available Routes
+app.use('/api/auth', require('./routes/auth'))
+app.use('/api/notes', require('./routes/notes'))
+
 app.get('/', (req, res) => {
-    res.send('Hello Jayesh!')
+  res.send('Hello Jayesh!')
 })
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port ${port}`)
 })
