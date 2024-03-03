8e7ddafb3369680ee1d1c8dff3d2574b16f2629f
pjayesh504 <pjayesh504@gmail.com>
2024-03-03 15:41:50 +0530

    Using React Context API

--- src/App.js
+++ src/App.js
@@ -3,17 +3,20 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import About from './components/About';
+import NoteState from './context/notes/NoteState';
 
 function App() {
   return (
     <>
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route exact path="/About" element={<About />} />
-        </Routes>
-      </Router>
+      <NoteState>
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route exact path="/About" element={<About />} />
+          </Routes>
+        </Router>
+      </NoteState>
     </>
   );
 }
--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,19 @@
-import React from 'react'
+import React, { useContext, useEffect } from 'react'
+import noteContext from '../context/notes/noteContext'
 
 const About = () => {
+  
+  const a = useContext(noteContext)
+
+  useEffect(() => {
+    a.update();
+    // eslint-disable-next-line
+  }, [])
+
   return (
-    <div>About</div>
+    <div>
+      This is About {a.state.name} and He is in class {a.state.class}
+    </div>
   )
 }
 
