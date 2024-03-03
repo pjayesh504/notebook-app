67e2faff364f455c98113bd7232550374336489a
pjayesh504 <pjayesh504@gmail.com>
2024-03-03 22:40:16 +0530

    Adding "fetch notes" functionality

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,18 @@
 const connectToMongo = require('./db');
-connectToMongo();
 const express = require('express')
+var cors = require('cors')
 
+connectToMongo();
 const app = express()
 const port = 5000
 
+app.use(cors())
 app.use(express.json())
 
-//Available Routes
+// Available Routes
 app.use('/api/auth', require('./routes/auth'))
 app.use('/api/notes', require('./routes/notes'))
 
-app.get('/', (req, res) => {
-  res.send('Hello Jayesh!')
-})
 
 app.listen(port, () => {
   console.log(`iNotebook backend listening at http://localhost:${port}`)
